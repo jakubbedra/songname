ad177fe5068fc1c051b76856870d207aa824b2a3
Jakub Bedra <s182437@o365.student.pg.edu.pl>
2021-10-20 23:42:15 +0200

    Added some stuff

--- src/main/java/com/konfyrm/songname/resource/AuthorsResource.java
+++ src/main/java/com/konfyrm/songname/resource/AuthorsResource.java
@@ -1,12 +1,17 @@
 package com.konfyrm.songname.resource;
 
 import com.konfyrm.songname.dto.GetAuthorsResponse;
+import com.konfyrm.songname.model.Author;
 import com.konfyrm.songname.service.AuthorsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("api/authors")
@@ -23,7 +28,10 @@ public class AuthorsResource {
 
     @GetMapping
     public ResponseEntity<GetAuthorsResponse> authors() {
-
+        List<Author> all = authorsService.getAllAuthors();
+        Function<Collection<Author>, GetAuthorsResponse> mapper = GetAuthorsResponse.entityToDtoMapper();
+        GetAuthorsResponse response = mapper.apply(all);
+        return ResponseEntity.ok(response);
     }
 
 }
